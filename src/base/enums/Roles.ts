395631e55eddc4ea3a42ceb60b540543ae34e666
unknown <luizschaly@gmail.com>
2024-11-06 21:54:32 -0300

    QOL

--- src/base/enums/Roles.ts
+++ src/base/enums/Roles.ts
@@ -1,7 +1,7 @@
 enum Roles {
     Support = "1298393843451826228",
     Admin = "1298252900929372214",
-    Founder = "1290347865331470378",
+    Founder = "1290347865331470378"
 }
 
 export default Roles
