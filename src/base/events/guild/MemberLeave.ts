0aa67ae5bd278fbf6df5bb361e8d7a4479659b31
Luiz Schaly <luizschaly@gmail.com>
2024-11-11 17:44:25 -0300

    Invite Exploit Fix

---
+++ src/base/events/guild/MemberLeave.ts
@@ -0,0 +1,34 @@
+import { ChatInputCommandInteraction, Events, GuildMember, Invite, User } from "discord.js";
+import CustomClient from "../../classes/CustomClient";
+import Event from "../../classes/Event";
+import inviteSchema from "../../schemas/invite";
+
+export default class MemberLeaveHandler extends Event {
+    constructor(client: CustomClient) {
+        super(client, {
+            //@ts-ignore
+            name: "memberLeave",
+            description: "Command handler event",
+            once: false
+
+        })
+    }
+    async Execute(member: GuildMember, inviter: User, invite: Invite): Promise<void> {
+        const role = member.guild.roles.cache.find(role => role.name === "Verified")
+        if (member.roles.cache.has(role!.id)) {
+            const inviteused = await inviteSchema.findOne({ InviteCode: invite.code, GuildID: member.guild.id })
+            if (inviteused?.UsersInvited.length == 1) {
+                inviteused!.UsersInvited = []
+            } else {
+                const index = inviteused!.UsersInvited.indexOf(member.id);
+                if (index !== -1) {
+                    inviteused?.UsersInvited.splice(index, 1);
+                }
+            }
+            await inviteSchema.updateOne({InviteCode: invite.code, GuildID: member.guild.id}, inviteused!)
+        
+        }
+
+    }
+
+}
