729f3c9d1e01ffb9df0dc9d316b96c766bb9a87e
Luiz Schaly <luizschaly@gmail.com>
2024-12-05 19:04:32 -0300

    Handle guild leave interactions

--- src/base/events/guild/MemberLeave.ts
+++ src/base/events/guild/MemberLeave.ts
@@ -1,7 +1,9 @@
-import { ChatInputCommandInteraction, Events, GuildMember, Invite, User } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder, Events, GuildMember, Invite, User } from "discord.js";
 import CustomClient from "../../classes/CustomClient";
 import Event from "../../classes/Event";
 import inviteSchema from "../../schemas/invite";
+import Channels from "../../enums/Channels";
+import Emojis from "../../enums/Emojis";
 
 export default class MemberLeaveHandler extends Event {
     constructor(client: CustomClient) {
@@ -28,6 +30,46 @@ export default class MemberLeaveHandler extends Event {
                 }
             }
             inviteused!.RealUses!--
+            const channel = await member.guild.channels.fetch(Channels.InvitesLogs)
+            const embed = new EmbedBuilder()
+            .setTitle("Member left")
+            .setFields(
+                {
+                  name: `${Emojis.BlurpleDot}Member Left`,
+                  value: `${Emojis.BlurpleArrow} <@${member.id}>`,
+                },
+                {
+                  name: `${Emojis.BlurpleDot}Invited By`,
+                  value: `${Emojis.BlurpleArrow} ${inviter.displayName || "Unknown"
+                    }`,
+                },
+                {
+                  name: `${Emojis.BlurpleDot}Invite Count`,
+                  value: `${Emojis.BlurpleArrow} ${invite!.uses! || "Unknown"}`,
+                },
+              );
+            //@ts-ignore
+            if(member.guild.vanityURLCode === inviter){
+                embed.addFields({
+                    name: `${Emojis.BlurpleDot}Invite Code`,
+                    value: `${Emojis.BlurpleArrow} ${invite.code}`,
+                  })
+            } else {
+                embed.addFields(
+                    {
+                      name: `${Emojis.BlurpleDot} ${inviter.displayName} Real Invites`,
+                      value: `${Emojis.BlurpleArrow} ${inviteused?.RealUses}`,
+                    }, {
+                    name: `${Emojis.BlurpleDot} ${inviter.displayName} Invites`,
+                    value: `${Emojis.BlurpleArrow} ${invite.uses}`,
+                  },
+                    {
+                      name: `${Emojis.BlurpleDot}Invite Code`,
+                      value: `${Emojis.BlurpleArrow} ${invite.code}`,
+                    })
+            }
+            //@ts-ignore
+            channel.send({embeds: [embed]})
             await inviteSchema.updateOne({InviteCode: invite.code, GuildID: member.guild.id}, inviteused!)
         
         }
